25645c78f2ddd78460c4f8d63b3d939a602b7739
Md. Shoyab <mdshoyab06@gmail.com>
2021-09-16 19:18:56 +0530

    Added splash page.

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,15 +1,18 @@
 import 'package:get/get.dart';
+
 import 'package:etelligens/app/modules/all_posts/bindings/all_posts_binding.dart';
 import 'package:etelligens/app/modules/all_posts/views/all_posts_view.dart';
 import 'package:etelligens/app/modules/post_details/bindings/post_details_binding.dart';
 import 'package:etelligens/app/modules/post_details/views/post_details_view.dart';
+import 'package:etelligens/app/modules/splash/bindings/splash_binding.dart';
+import 'package:etelligens/app/modules/splash/views/splash_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.ALL_POSTS;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
@@ -21,6 +24,11 @@ class AppPages {
       name: _Paths.ALL_POSTS,
       page: () => AllPostsView(),
       binding: AllPostsBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashView(),
+      binding: SplashBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,9 +5,11 @@ abstract class Routes {
   Routes._();
   static const POST_DETAILS = _Paths.POST_DETAILS;
   static const ALL_POSTS = _Paths.ALL_POSTS;
+  static const SPLASH = _Paths.SPLASH;
 }
 
 abstract class _Paths {
   static const POST_DETAILS = '/post-details';
   static const ALL_POSTS = '/all-posts';
+  static const SPLASH = '/splash';
 }
